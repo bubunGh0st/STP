b7e9e576c6d6dce777fbd2e13f2809f49f4b5dc8
bubunGh0st <38558914+bubunGh0st@users.noreply.github.com>
2018-04-21 15:48:57 +1200

    Changed the footer.php

--- application/views/templates-nosession/footer.php
+++ application/views/templates-nosession/footer.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Footer -->
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Student Time Planner 2018</p>
+        <p class="m-0 text-center text-white">Copyright &copy; Student Time Planner(Fuck You Bitch) 2018</p>
       </div>
       <!-- /.container -->
     </footer>
